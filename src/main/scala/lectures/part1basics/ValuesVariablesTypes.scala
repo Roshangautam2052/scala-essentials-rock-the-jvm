7b9730efe867baf2fd24c85ce84efddab2d03f16
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-04-05 11:16:00 +0100

    Initialized project

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,10 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: String = "HELLO SCALA "
+  println(x)
+
+  // VALS ARE IMMUTABLE
+  // COMPILER HAVE INFERRED TYPES
+}
